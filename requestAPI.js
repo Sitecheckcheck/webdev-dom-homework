3e3308bbdceb6444e96e35c47a51fd920d9be10a
Pavel <pvdanilov88@inbox.ru>
2023-04-02 17:15:08 +0300

    ДЗ 2.15

---
+++ requestAPI.js
@@ -0,0 +1,25 @@
+export function apiGet() {
+  return fetch(
+    "https://webdev-hw-api.vercel.app/api/v1/pavel-danilov/comments",
+    {
+      method: "GET",
+    }
+  ).then((response) => {
+    return response.json();
+  });
+}
+
+
+export function apiPost(textValue, nameValue) {
+  return fetch(
+    "https://webdev-hw-api.vercel.app/api/v1/pavel-danilov/comments",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        text: textValue,
+        name: nameValue,
+        forceError: false,
+      }),
+    }
+  );
+}
